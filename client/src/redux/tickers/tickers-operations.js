420717e0acf3fba4cb013a1d28e7334780dd931a
deoneshka <oleg.deonega@gmail.com>
2021-11-16 12:35:59 +0200

    Update tickers-operations.js

--- client/src/redux/tickers/tickers-operations.js
+++ client/src/redux/tickers/tickers-operations.js
@@ -7,12 +7,15 @@ import socket from '../../socket/socket';
 
 const fetchTickers = () => dispatch => {
   dispatch(initTickersRequest());
+
   socket.on('connect', function () {
+    dispatch(initTickersRequest());
     socket.emit('start');
     socket.on('ticker', tickers => dispatch(initTickersSuccess(tickers)));
   });
 
   socket.on('connect_error', function () {
+    dispatch(initTickersRequest());
     dispatch(initTickersError());
   });
 };
